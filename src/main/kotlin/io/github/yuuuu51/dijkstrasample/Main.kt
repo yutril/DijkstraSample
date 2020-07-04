315e3ebeaf59fec1dbb9afcf9da66bcf7398db2f
yuuuu51 <bboy.yu510@gmail.com>
2020-07-04 23:04:21 +0900

    update

--- src/main/kotlin/io/github/yuuuu51/dijkstrasample/Main.kt
+++ src/main/kotlin/io/github/yuuuu51/dijkstrasample/Main.kt
@@ -1,5 +1,41 @@
 package io.github.yuuuu51.dijkstrasample
 
-fun main(args: Array<String>) {
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Klaxon
+import io.github.yuuuu51.dijkstralib.DijkstraLib
+import io.github.yuuuu51.dijkstralib.element.Edge
+import io.github.yuuuu51.dijkstralib.element.ElementFactory
+import io.github.yuuuu51.dijkstralib.element.Node
+import java.nio.file.Files
+import java.nio.file.Paths
 
+const val START_NODE_ID = "1"
+const val GOAL_NODE_ID = "8"
+
+fun main(args: Array<String>) {
+    val edges = mutableMapOf<String, Edge>()
+    loadJson("/edges.json").forEach {(id, length) ->
+        check(length is Int)
+        edges[id] = ElementFactory.createEdge(id, length)
+    }
+    val nodes = mutableMapOf<String, Node>()
+    loadJson("/nodes.json").forEach {(id, data) ->
+        check(data is List<*>)
+        val node = ElementFactory.createNode(id)
+        data.forEach {
+            check(it is String)
+            edges[it]?.let { it1 -> node.addEdge(it1) }
+        }
+        nodes[id] = node
+    }
+    val dijkstra = DijkstraLib(nodes, edges)
+    dijkstra.execute(START_NODE_ID, GOAL_NODE_ID).forEach {
+        println("Node:${it.id}")
+    }
+}
+
+fun loadJson(path: String): JsonObject {
+    val klaxon = Klaxon()
+    val br = Files.newBufferedReader(Paths.get(klaxon::class.java.getResource(path).path))
+    return klaxon.parseJsonObject(br)
 }
